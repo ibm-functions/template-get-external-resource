aab42f13b27e3a4f3de0a76e33a0ef4d93d2aa69
Belinda V <beemarie@Belindas-MacBook-Pro.local>
2017-12-27 18:06:04 -0600

    url break in two lines

--- runtimes/nodejs/actions/weather.js
+++ runtimes/nodejs/actions/weather.js
@@ -16,8 +16,8 @@ var request = require('request');
 
 function main(params) {
     var location = params.location || 'Vermont';
-    var url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json';
-
+    var url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast \
+        where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json';
     return new Promise(function(resolve, reject) {
         request.get(url, function(error, response, body) {
             if (error) {
