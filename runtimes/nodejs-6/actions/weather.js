c30a68bca3f160a9dcb19747a1980b705af0d722
Belinda Vennam <beemarie@us.ibm.com>
2018-02-14 14:19:26 -0600

    updating js actions

--- runtimes/nodejs-6/actions/weather.js
+++ runtimes/nodejs-6/actions/weather.js
@@ -15,33 +15,32 @@
 const request = require('request');
 
 function main(params) {
-    const location = params.location || 'Vermont';
-    const url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast \
-        where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json';
-    return new Promise(function(resolve, reject) {
-        request.get(url, function(error, response, body) {
-            if (error) {
-                reject({
-                    statusCode: 500,
-                    headers: { 'Content-Type': 'application/json' },
-                    body: {'message': 'Error processing your request'}
-                });
-            }
-            else {
-                /** The response body contains temperature data in the following format
-                 *    { code: '28',
-                 *    date: 'Tue, 26 Dec 2017 12:00 PM EST',
-                 *    temp: '18',
-                 *    text: 'Mostly Cloudy' } }
-                  */
-                resolve({
-                    statusCode: 200,
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.parse(body).query.results.channel.item.condition
-                });
-            }
+  const location = params.location || 'Vermont';
+  const url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast \
+    where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json';
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject({
+          statusCode: 500,
+          headers: { 'Content-Type': 'application/json' },
+          body: {'message': 'Error processing your request' },
         });
+      } else {
+        /** The response body contains temperature data in the following format
+         *    { code: '28',
+         *    date: 'Tue, 26 Dec 2017 12:00 PM EST',
+         *    temp: '18',
+         *    text: 'Mostly Cloudy' } }
+         */
+        resolve({
+          statusCode: 200,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.parse(body).query.results.channel.item.condition,
+        });
+      }
     });
+  });
 }
 
 exports.main = main;
--- runtimes/nodejs-8/actions/weather.js
+++ runtimes/nodejs-8/actions/weather.js
@@ -16,26 +16,31 @@ const { promisify } = require('util');
 const request = promisify(require('request'));
 
 async function main(params) {
-  let {
+  const {
     location = 'Vermont',
-    url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast \
-        where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json'
-  } = params
-  let response
+  } = params;
+  let response;
+  const url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast \
+    where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json'
   try {
-    response = await request(url)
+    response = await request(url);
   } catch (err) {
     return Promise.reject({
       statusCode: 500,
       headers: { 'Content-Type': 'application/json' },
-      body: {'message': 'Error processing your request'}
-    })
+      body: { message: 'Error processing your request' },
+    });
   }
+  /** The response body contains temperature data in the following format
+   *    { code: '28',
+   *    date: 'Tue, 26 Dec 2017 12:00 PM EST',
+   *    temp: '18',
+   *    text: 'Mostly Cloudy' } }
+   */
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.parse(response.body).query.results.channel.item.condition
-  }
+    body: JSON.parse(response.body).query.results.channel.item.condition,
+  };
 }
-
 exports.main = main;
