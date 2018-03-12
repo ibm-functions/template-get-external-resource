dbd55968679841b8f3b225c73a8ee1a1247c56b4
Belinda Vennam <beemarie@us.ibm.com>
2018-03-11 23:28:58 -0500

    format

--- tests/src/test/scala/templates/WeatherTests.scala
+++ tests/src/test/scala/templates/WeatherTests.scala
@@ -28,206 +28,207 @@ import com.jayway.restassured.RestAssured
 import com.jayway.restassured.config.SSLConfig
 import spray.json.DefaultJsonProtocol.StringJsonFormat
 import spray.json._
-
 import spray.json.DefaultJsonProtocol._
 
 @RunWith(classOf[JUnitRunner])
 class WeatherTests extends TestHelpers
-    with WskTestHelpers
-    with BeforeAndAfterAll {
+  with WskTestHelpers
+  with BeforeAndAfterAll {
 
-    implicit val wskprops = WskProps()
-    val wsk = new Wsk()
+  implicit val wskprops = WskProps()
+  val wsk = new Wsk()
 
-    val deployTestRepo = "https://github.com/ibm-functions/template-get-external-resource"
-    val getExternalResourceActionPackage = "myPackage/weather"
-    val deployAction = "/whisk.system/deployWeb/wskdeploy"
-    val deployActionURL = s"https://${wskprops.apihost}/api/v1/web${deployAction}.http"
-    val packageName = "myPackage"
+  val deployTestRepo = "https://github.com/ibm-functions/template-get-external-resource"
+  val getExternalResourceAction = "myPackage/weather"
+  val deployAction = "/whisk.system/deployWeb/wskdeploy"
+  val deployActionURL = s"https://${wskprops.apihost}/api/v1/web${deployAction}.http"
+  val packageName = "myPackage"
 
-    //set parameters for deploy tests
-    val node8RuntimePath = "runtimes/nodejs"
-    val nodejs8folder = "../runtimes/nodejs/actions";
-    val nodejs8kind = "nodejs:8"
-    val node6RuntimePath = "runtimes/nodejs-6"
-    val nodejs6folder = "../runtimes/nodejs-6/actions";
-    val nodejs6kind = "nodejs:6"
-    val pythonRuntimePath = "runtimes/python"
-    val pythonfolder = "../runtimes/python/actions";
-    val pythonkind = "python-jessie:3"
+  //set parameters for deploy tests
+  val node8RuntimePath = "runtimes/nodejs"
+  val nodejs8folder = "../runtimes/nodejs/actions";
+  val nodejs8kind = "nodejs:8"
+  val node6RuntimePath = "runtimes/nodejs-6"
+  val nodejs6folder = "../runtimes/nodejs-6/actions";
+  val nodejs6kind = "nodejs:6"
+  val pythonRuntimePath = "runtimes/python"
+  val pythonfolder = "../runtimes/python/actions";
+  val pythonkind = "python-jessie:3"
 
-    // statuses from deployWeb
-    val successStatus = """"status":"success""""
+  // statuses from deployWeb
+  val successStatus = """"status":"success""""
 
-    behavior of "Get External Resource Template"
+  behavior of "Get External Resource Template"
 
-    // test to create the get external resource template from github url.  Will use preinstalled folder.
-    it should "create the nodejs 8 get external resource action from github url" in {
-      makePostCallWithExpectedResult(JsObject(
-        "gitUrl" -> JsString(deployTestRepo),
-        "manifestPath" -> JsString(node8RuntimePath),
-        "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
-        "wskApiHost" -> JsString(wskprops.apihost),
-        "wskAuth" -> JsString(wskprops.authKey)
-      ), successStatus, 200);
+  // test to create the get external resource template from github url.  Will use preinstalled folder.
+  it should "create the nodejs 8 get external resource action from github url" in {
+    makePostCallWithExpectedResult(JsObject(
+      "gitUrl" -> JsString(deployTestRepo),
+      "manifestPath" -> JsString(node8RuntimePath),
+      "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
+      "wskApiHost" -> JsString(wskprops.apihost),
+      "wskAuth" -> JsString(wskprops.authKey)
+    ), successStatus, 200);
 
-      withActivation(wsk.activation, wsk.action.invoke(getExternalResourceActionPackage)) {
-        _.response.result.get.toString should include("temp")
-      }
-
-      val action = wsk.action.get(getExternalResourceActionPackage)
-      verifyAction(action, getExternalResourceActionPackage, JsString(nodejs8kind))
-
-      // clean up after test
-      wsk.action.delete(getExternalResourceActionPackage)
+    withActivation(wsk.activation, wsk.action.invoke(getExternalResourceAction)) {
+      _.response.result.get.toString should include("temp")
     }
 
-    // test to create the get external resource template from github url.  Will use preinstalled folder.
-    it should "create the nodejs 6 get external resource action from github url" in {
-      makePostCallWithExpectedResult(JsObject(
-        "gitUrl" -> JsString(deployTestRepo),
-        "manifestPath" -> JsString(node6RuntimePath),
-        "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
-        "wskApiHost" -> JsString(wskprops.apihost),
-        "wskAuth" -> JsString(wskprops.authKey)
-      ), successStatus, 200);
+    val action = wsk.action.get(getExternalResourceAction)
+    verifyAction(action, getExternalResourceAction, JsString(nodejs8kind))
 
-      withActivation(wsk.activation, wsk.action.invoke(getExternalResourceActionPackage)) {
-        _.response.result.get.toString should include("temp")
-      }
+    // clean up after test
+    wsk.action.delete(getExternalResourceAction)
+  }
 
-      val action = wsk.action.get(getExternalResourceActionPackage)
-      verifyAction(action, getExternalResourceActionPackage, JsString(nodejs6kind))
+  // test to create the get external resource template from github url.  Will use preinstalled folder.
+  it should "create the nodejs 6 get external resource action from github url" in {
+    makePostCallWithExpectedResult(JsObject(
+      "gitUrl" -> JsString(deployTestRepo),
+      "manifestPath" -> JsString(node6RuntimePath),
+      "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
+      "wskApiHost" -> JsString(wskprops.apihost),
+      "wskAuth" -> JsString(wskprops.authKey)
+    ), successStatus, 200);
 
-      // clean up after test
-      wsk.action.delete(getExternalResourceActionPackage)
+    withActivation(wsk.activation, wsk.action.invoke(getExternalResourceAction)) {
+      _.response.result.get.toString should include("temp")
     }
 
-    // test to create the get external resource template from github url.  Will use preinstalled folder.
-    it should "create the python get external resource action from github url" in {
-      makePostCallWithExpectedResult(JsObject(
-        "gitUrl" -> JsString(deployTestRepo),
-        "manifestPath" -> JsString(pythonRuntimePath),
-        "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
-        "wskApiHost" -> JsString(wskprops.apihost),
-        "wskAuth" -> JsString(wskprops.authKey)
-      ), successStatus, 200);
+    val action = wsk.action.get(getExternalResourceAction)
+    verifyAction(action, getExternalResourceAction, JsString(nodejs6kind))
 
-      withActivation(wsk.activation, wsk.action.invoke(getExternalResourceActionPackage)) {
-        _.response.result.get.toString should include("temp")
-      }
+    // clean up after test
+    wsk.action.delete(getExternalResourceAction)
+  }
 
-      val action = wsk.action.get(getExternalResourceActionPackage)
-      verifyAction(action, getExternalResourceActionPackage, JsString(pythonkind))
+  // test to create the get external resource template from github url.  Will use preinstalled folder.
+  it should "create the python get external resource action from github url" in {
+    makePostCallWithExpectedResult(JsObject(
+      "gitUrl" -> JsString(deployTestRepo),
+      "manifestPath" -> JsString(pythonRuntimePath),
+      "envData" -> JsObject("PACKAGE_NAME" -> JsString(packageName)),
+      "wskApiHost" -> JsString(wskprops.apihost),
+      "wskAuth" -> JsString(wskprops.authKey)
+    ), successStatus, 200);
 
-      // clean up after test
-      wsk.action.delete(getExternalResourceActionPackage)
-    }
-    /**
-     * Test the nodejs 6 "Get External Resource" template
-     */
-     it should "invoke nodejs-6 weather.js and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-       val name = "weatherNode6"
-       val file = Some(new File(nodejs6folder, "weather.js").toString());
-       assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-         action.create(name, file, kind = Some(nodejs6kind))
-       }
-
-       withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
-         activation =>
-          activation.response.success shouldBe true
-          activation.response.result.get.toString should include("temp")
-       }
-     }
-     it should "invoke nodejs-6 weather.js without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-        val name = "weatherNode6"
-        val file = Some(new File(nodejs6folder, "weather.js").toString());
-        assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-          action.create(name, file, kind = Some(nodejs6kind))
-        }
-
-        withActivation(wsk.activation, wsk.action.invoke(name)) {
-          activation =>
-           activation.response.success shouldBe true
-           activation.response.result.get.toString should include("temp")
-        }
-      }
-
-    /**
-     * Test the nodejs-8 "Get External Resource" template
-     */
-     it should "invoke nodejs-8 weather.js and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-       val name = "weatherNode8"
-       val file = Some(new File(nodejs8folder, "weather.js").toString());
-       assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-         action.create(name, file, kind = Some(nodejs8kind))
-       }
-
-       withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
-         activation =>
-          activation.response.success shouldBe true
-          activation.response.result.get.toString should include("temp")
-       }
-     }
-      it should "invoke nodejs-8 weather.js without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-        val name = "weatherNode8"
-        val file = Some(new File(nodejs8folder, "weather.js").toString());
-        assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-          action.create(name, file, kind = Some(nodejs8kind))
-        }
-
-        withActivation(wsk.activation, wsk.action.invoke(name)) {
-          activation =>
-           activation.response.success shouldBe true
-           activation.response.result.get.toString should include("temp")
-        }
-      }
-
-    /**
-     * Test the python "Get External Resource" template
-     */
-     it should "invoke weather.py and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-       val name = "weatherPython"
-       val file = Some(new File(pythonfolder, "weather.py").toString());
-       assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-         action.create(name, file, kind = Some(pythonkind))
-       }
-
-       withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
-         activation =>
-          activation.response.success shouldBe true
-          activation.response.result.get.toString should include("temp")
-        }
-     }
-      it should "invoke weather.py without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
-        val name = "weatherPython"
-        val file = Some(new File(pythonfolder, "weather.py").toString());
-        assetHelper.withCleaner(wsk.action, name) { (action, _) =>
-          action.create(name, file, kind = Some(pythonkind))
-        }
-
-        withActivation(wsk.activation, wsk.action.invoke(name)) {
-          activation =>
-           activation.response.success shouldBe true
-           activation.response.result.get.toString should include("temp")
-        }
-      }
-
-
-    private def makePostCallWithExpectedResult(params: JsObject, expectedResult: String, expectedCode: Int) = {
-      val response = RestAssured.given()
-          .contentType("application/json\r\n")
-          .config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
-          .body(params.toString())
-          .post(deployActionURL)
-      assert(response.statusCode() == expectedCode)
-      response.body.asString should include(expectedResult)
-      response.body.asString.parseJson.asJsObject.getFields("activationId") should have length 1
+    withActivation(wsk.activation, wsk.action.invoke(getExternalResourceAction)) {
+      _.response.result.get.toString should include("temp")
     }
 
-    private def verifyAction(action: RunResult, name: String, kindValue: JsString): Unit = {
-      val stdout = action.stdout
-      assert(stdout.startsWith(s"ok: got action $name\n"))
-      wsk.parseJsonString(stdout).fields("exec").asJsObject.fields("kind") shouldBe kindValue
+    val action = wsk.action.get(getExternalResourceAction)
+    verifyAction(action, getExternalResourceAction, JsString(pythonkind))
+
+    // clean up after test
+    wsk.action.delete(getExternalResourceAction)
+  }
+  /**
+    * Test the nodejs 6 "Get External Resource" template
+    */
+  it should "invoke nodejs-6 weather.js and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherNode6"
+    val file = Some(new File(nodejs6folder, "weather.js").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(nodejs6kind))
     }
+
+    withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+
+  it should "invoke nodejs-6 weather.js without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherNode6"
+    val file = Some(new File(nodejs6folder, "weather.js").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(nodejs6kind))
+    }
+
+    withActivation(wsk.activation, wsk.action.invoke(name)) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+
+  /**
+    * Test the nodejs-8 "Get External Resource" template
+    */
+  it should "invoke nodejs-8 weather.js and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherNode8"
+    val file = Some(new File(nodejs8folder, "weather.js").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(nodejs8kind))
+    }
+
+    withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+
+  it should "invoke nodejs-8 weather.js without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherNode8"
+    val file = Some(new File(nodejs8folder, "weather.js").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(nodejs8kind))
+    }
+
+    withActivation(wsk.activation, wsk.action.invoke(name)) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+
+  /**
+    * Test the python "Get External Resource" template
+    */
+  it should "invoke weather.py and get the result" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherPython"
+    val file = Some(new File(pythonfolder, "weather.py").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(pythonkind))
+    }
+
+    withActivation(wsk.activation, wsk.action.invoke(name, Map("location" -> "Paris".toJson))) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+  it should "invoke weather.py without input and get weather for Vermont" in withAssetCleaner(wskprops) { (wp, assetHelper) =>
+    val name = "weatherPython"
+    val file = Some(new File(pythonfolder, "weather.py").toString());
+    assetHelper.withCleaner(wsk.action, name) { (action, _) =>
+      action.create(name, file, kind = Some(pythonkind))
+    }
+
+    withActivation(wsk.activation, wsk.action.invoke(name)) {
+      activation =>
+        activation.response.success shouldBe true
+        activation.response.result.get.toString should include("temp")
+    }
+  }
+
+
+  private def makePostCallWithExpectedResult(params: JsObject, expectedResult: String, expectedCode: Int) = {
+    val response = RestAssured.given()
+      .contentType("application/json\r\n")
+      .config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+      .body(params.toString())
+      .post(deployActionURL)
+    assert(response.statusCode() == expectedCode)
+    response.body.asString should include(expectedResult)
+    response.body.asString.parseJson.asJsObject.getFields("activationId") should have length 1
+  }
+
+  private def verifyAction(action: RunResult, name: String, kindValue: JsString): Unit = {
+    val stdout = action.stdout
+    assert(stdout.startsWith(s"ok: got action $name\n"))
+    wsk.parseJsonString(stdout).fields("exec").asJsObject.fields("kind") shouldBe kindValue
+  }
 }
